bab528c2c12c3696ff8e026f4866cad83f669c38
Mahesh5726 <maheshr070204@gmail.com>
2025-05-05 12:06:57 +0530

    Add Radix UI Button component and update RootPage layout with new design

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
-"use client";
-
-import { betterAuthClient } from "@/lib/integrations/better-auth";
+import { Button } from "@/components/ui/button";
 
 const RootPage = () => {
-  const { data } = betterAuthClient.useSession();
   return (
-    <div>RootPage</div>
-  )
-}
+    <div className="h-svh container mx-auto">
+      <div className="h-full flex flex-col items-center justify-center gap-6">
+        <div className="flex flex-col items-center justify-center gap-0.5">
+          <h1 className="text-4xl font-extrabold text-balance max-w-prose">
+            Platinum
+          </h1>
+          <p className="max-w-prose">
+            Platinum is a social media platform for the elite.
+          </p>
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <Button>Sign Up</Button>
+          <Button variant="secondary">Log In</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RootPage
+export default RootPage;
